a0dba8df2ea9724b37a84f02bdf8bb3c2d2c4c16
Eatkin <67864150+Eatkin@users.noreply.github.com>
2023-06-14 19:38:41 +0100

    Replace var with let

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
 let styleElement = document.createElement('style');
 styleElement.textContent = 'body {font-family: "Comic Sans MS", "Comic Sans", cursive; !important;}'
-var head = document.head || document.getElementsByTagName('head')[0];
+let head = document.head || document.getElementsByTagName('head')[0];
 head.appendChild(styleElement);
